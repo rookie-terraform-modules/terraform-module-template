4bf877597f5b99be17e1aa45ae6f8cc17e9ccdec
Oluwatobi Ogunbayo <tobyogunbayo@gmail.com>
2022-12-09 15:02:03 +0100

    feat: add extra input variable

--- vars.tf
+++ vars.tf
@@ -3,3 +3,8 @@ variable "tags" {
   default     = {}
   description = "Tags to apply to all resources"
 }
+
+variable "name_prefix" {
+  type        = string
+  description = "Prefix to apply to all resources"
+}
